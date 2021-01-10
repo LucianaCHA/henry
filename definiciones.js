66f2c9b47e1e171130651b6446df84606a625fa8
LucianaCHA <lucianahamorro87@gmail.com>
2021-01-10 17:42:51 -0300

    definiciones de conceptos ok

---
+++ definiciones.js
@@ -0,0 +1,25 @@
+Definiciones
+# Variables: es un elemento que define algo, puede ser un valor,
+nombre, textocualquier cosa que pueda ser definida y que me interese indicar o procesar.
+De acuerdo a lo anterior las variables pueden contener datos dediversos tipos:
+
+#STRING:  es tipo de dato  que consiste en texto y que se define entre comillas
+
+#NUMBER: son números
+
+#BOOLEAN: Es un tipo de dato que tiene dos opciones TRUE/FALSE.
+#puede estar asociado al ingreso de info o cumpliir cierta condición
+# tal que si recibo lo esperado sea TRUE en caso opuesto será FALSE
+
+# FUNCIONES: Son instrucciones que indican como tratar los datos recibidos
+(argumentos)para devolver un determinado resultado o correr algún tipo
+de proceso sobre el mismo, siempre y cuando los datos sean procesables.
+ Por ejmplo, si mi función está programada para ´´incrementar en uno
+ cualquier número´´ hará precisamente eso cuando reciba un argumento numérico
+
+#DECLARACIONES: identifican las funciones en el codigo y la definen.
+ Se forman por la  keyword fuction + nombre de la función+ declaración. Por ej.
+ la declaraciión if, indica a la función que una condición debe ser cumplida
+ para cursar la operación.
+
+ 
